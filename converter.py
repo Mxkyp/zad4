c43a00d5790c80adb82c6cb826cdf709797120bb
mxkcpy <mikolaj19@poczta.fm>
2025-05-02 18:46:47 +0200

    seperated into methods

--- converter.py
+++ converter.py
@@ -2,24 +2,34 @@ import sounddevice as sd
 import numpy as np
 from scipy.io.wavfile import write
 
+# Parametry nagrywania
+SAMPLE_RATE = 44100  # częstotliwość próbkowania
+CHANNELS = 1  # liczba kanałów (mono)
+DURATION = 5  # czas nagrania w sekundach
 
-def quantinize(audio_data: np.ndarray):
-    # audio_data to tablica shape=(N,channels); spłaszczamy do 1D
-    audio_data = audio_data.flatten()
-    # Kwantyzacja (zmniejszenie do 8 bitów)
-    # dzielimy przez 256, zaokrąglamy, mnożymy z powrotem
-    quantized_audio = np.round(audio_data / 256) * 256
+
+def max_unsigned(bits):
+    return 2**bits
+
+
+def quantinize(audio_data: np.ndarray, bitResolution):
+    # Kwantyzacja (zmniejszenie do bitResolution bitów)
+    quantized_audio = np.round(audio_data / max_unsigned(bitResolution)) * max_unsigned(
+        bitResolution
+    )
     quantized_audio = quantized_audio.astype(np.int16)
     return quantized_audio
 
+    # audio_data to tablica shape=(N,channels); spłaszczamy do 1D
 
-def record():
+
+def record(dtype: str):
     print("Nagrywanie...")
     audio_data = sd.rec(
         int(DURATION * SAMPLE_RATE),
         samplerate=SAMPLE_RATE,
         channels=CHANNELS,
-        dtype=DTYPE,
+        dtype=dtype,
     )
     sd.wait()  # czekaj na zakończenie nagrania
     print("Nagrywanie zakończone.")
@@ -32,21 +42,14 @@ def calculate_snr(original, quantized):
     return 10 * np.log10(signal_energy / noise_energy)
 
 
-# Parametry nagrywania
-SAMPLE_RATE = 44100  # częstotliwość próbkowania
-CHANNELS = 1  # liczba kanałów (mono)
-DURATION = 1  # czas nagrania w sekundach
-DTYPE = "int16"  # 16-bitowe próbki
-
 # Nagrywanie dźwięku
-audio_data = record()
-quantized_audio = quantinize(audio_data)
+audio_data = record("int16")
+quantized_audio = quantinize(audio_data, 8)
 
 # Zapis do WAV
 write("original_audio.wav", SAMPLE_RATE, audio_data)
 write("quantized_audio.wav", SAMPLE_RATE, quantized_audio)
 print("Pliki WAV zapisane: original_audio.wav, quantized_audio.wav")
 
-# Obliczenie SNR
 snr_value = calculate_snr(audio_data, quantized_audio)
 print(f"SNR: {snr_value:.2f} dB")
